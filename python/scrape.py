d5c9bd1062356b862ebd5ab4e07cb742b4d38425
Caleb Easterly <caleb.easterly@gmail.com>
2022-11-14 12:33:40 -0500

    for keeping track of progress chunk urls

--- python/scrape.py
+++ python/scrape.py
@@ -1,5 +1,6 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, set_start_method
 import pandas as pd
+import numpy as np
 import os
 import argparse
 import pickle
@@ -10,7 +11,18 @@ from campaign import scrape_campaign
 from get_sitemap import get_sitemap
 from dump_xmls import dump_xmls
 
+def chunks(lst, n):
+    """
+    Yield successive n-sized chunks
+    From https://stackoverflow.com/questions/312443/how-do-i-split-a-list-into-equally-sized-chunks
+    """
+    for i in range(0, len(lst), n):
+            yield lst[i:i + n]
+
 if __name__ == '__main__':
+    # spawn instead of fork
+    set_start_method("spawn")
+
     # command line management
     parser = argparse.ArgumentParser(description='GoFundMe web scraper')
     parser.add_argument('-d', dest='dir', default=".", help="data directory")
@@ -40,19 +52,43 @@ if __name__ == '__main__':
     # set seed for language detection for reproducibility
     DetectorFactory.seed = 10432987
 
+    # split urls into chunks of 10000 - should give about two hundred files
+    chunksize = 10000
+    if args.test:
+        chunksize = 10
+    chunked_urls = list(chunks(urls, chunksize))
+    nchunk = len(chunked_urls)
+
     # do the scrape
     ncore = int(args.ncore)
-    with Pool(processes=ncore) as pool:
-        campaigns = pool.map(scrape_campaign, urls)
+    # list to hold all chunked dataframes
+    dfs = []
+    for i in range(nchunk):
+        with Pool(processes=ncore) as pool:
+            chunk = chunked_urls[i]
+            campaigns = pool.map(scrape_campaign, chunk)
 
-    # combine results into big dataframe
-    df = pd.concat(campaigns, ignore_index=True)
+            # combine results into big dataframe
+            df = pd.concat(campaigns, ignore_index=True)
 
-    # output results both as csv and pickle
+            # output results both as csv and pickle
+            outf_pickle = os.path.join(data_dir, args.outf) + str(i) + ".pkl"
+            with open(outf_pickle, "wb") as fp:
+                pickle.dump(df, fp)
+
+            outf_csv = os.path.join(data_dir, args.outf) + str(i) + ".csv"
+            with open(outf_csv, "w", encoding="utf-8") as fc:
+                df.to_csv(fc, index=False, line_terminator='\n', encoding='utf-8')
+
+            # storing for writing later
+            dfs.append(df)
+
+    # combine the files
+    full_df = pd.concat(dfs, ignore_index=True)
     outf_pickle = os.path.join(data_dir, args.outf) + ".pkl"
     with open(outf_pickle, "wb") as fp:
-        pickle.dump(df, fp)
+        pickle.dump(full_df, fp)
 
     outf_csv = os.path.join(data_dir, args.outf) + ".csv"
     with open(outf_csv, "w", encoding="utf-8") as fc:
-        df.to_csv(fc, index=False, line_terminator='\n', encoding='utf-8')
+        full_df.to_csv(fc, index=False, line_terminator='\n', encoding='utf-8')

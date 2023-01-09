239602550353a3e5d7e7b8b43df4bc630fdbea9b
Caleb Easterly <caleb.easterly@gmail.com>
2023-01-09 13:15:32 -0500

    python notebooks for creating analytic file

--- python/analyze_scrape.py
+++ python/analyze_scrape.py
@@ -9,41 +9,44 @@ import pickle
 from multiprocessing import Pool
 
 # change directory
-os.chdir("C:\\Users\\caleb\\OneDrive - University of North Carolina at Chapel Hill\\Documents\\Projects\\Cancer care crowdfunding\\GoFundMeUU")
+os.chdir("C:\\Users\\caleb\\OneDrive - University of North Carolina at Chapel Hill\\Documents\\Projects\\Cancer care crowdfunding")
 
-headers = ["URL", "Title", "Status", "Location", "Raised", "Goal", 
-    "Number_of_Donors", "Date_Created", "Tag", "Description", "Language1", "Language2", "InEnglish"]
-scraped = pd.DataFrame(columns = headers)
+# examine end of file (todo-read file in chunks for filtering)
+df = pd.read_csv("results_11.12.22\\walk_soft\\scraped.csv", skiprows = 2200000)
+df.to_csv("results_11.12.22\\walk_soft\\scraped_tail.csv", index=False, line_terminator="\n", encoding="utf-8")
+# headers = ["URL", "Title", "Status", "Location", "Raised", "Goal", 
+#     "Number_of_Donors", "Date_Created", "Tag", "Description", "Language1", "Language2", "InEnglish"]
+# scraped = pd.DataFrame(columns = headers)
 
-def extract_to_series(c):
-    crow = pd.DataFrame(columns=headers)
-    crow.loc[0, "URL"] = c.url
-    crow.loc[0, "Title"] = c.campaign_title
-    crow.loc[0, "Status"] = c.campaign_status
-    crow.loc[0, "Location"] = c.location_string
-    crow.loc[0, "State"] = c.state
-    crow.loc[0, "IsInUS"] = c.is_us_state_code
-    crow.loc[0, "Raised"] = c.raised
-    crow.loc[0, "Goal"] = c.goal
-    crow.loc[0, "Number_of_Donors"] = c.num_donors
-    crow.loc[0, "Date_Created"] = c.date_created
-    crow.loc[0, "Tag"] = c.tag
-    crow.loc[0, "Description"] = c.campaign_desc
-    crow.loc[0, "Language1"] = c.lang1
-    crow.loc[0, "Language2"] = c.lang2
-    crow.loc[0, "InEnglish"] = c.in_english
-    return crow
+# def extract_to_series(c):
+#     crow = pd.DataFrame(columns=headers)
+#     crow.loc[0, "URL"] = c.url
+#     crow.loc[0, "Title"] = c.campaign_title
+#     crow.loc[0, "Status"] = c.campaign_status
+#     crow.loc[0, "Location"] = c.location_string
+#     crow.loc[0, "State"] = c.state
+#     crow.loc[0, "IsInUS"] = c.is_us_state_code
+#     crow.loc[0, "Raised"] = c.raised
+#     crow.loc[0, "Goal"] = c.goal
+#     crow.loc[0, "Number_of_Donors"] = c.num_donors
+#     crow.loc[0, "Date_Created"] = c.date_created
+#     crow.loc[0, "Tag"] = c.tag
+#     crow.loc[0, "Description"] = c.campaign_desc
+#     crow.loc[0, "Language1"] = c.lang1
+#     crow.loc[0, "Language2"] = c.lang2
+#     crow.loc[0, "InEnglish"] = c.in_english
+#     return crow
 
-fpath = "C:\\Users\\caleb\\OneDrive - University of North Carolina at Chapel Hill\\Documents\\Projects\\Cancer care crowdfunding\\results_11.12.22\\t2\\c3lgbt-data\\scraped.px"
-with open(fpath, "rb") as f:
-    campaigns = pickle.load(f)
-    campaigns = campaigns[:10000]
-    # with Pool(processes=4) as pool:
-    rows = [extract_to_series(c) for c in campaigns]
-    df = pd.concat(rows, ignore_index=True)
+# fpath = "C:\\Users\\caleb\\OneDrive - University of North Carolina at Chapel Hill\\Documents\\Projects\\Cancer care crowdfunding\\results_11.12.22\\t2\\c3lgbt-data\\scraped.px"
+# with open(fpath, "rb") as f:
+#     campaigns = pickle.load(f)
+#     campaigns = campaigns[:10000]
+#     # with Pool(processes=4) as pool:
+#     rows = [extract_to_series(c) for c in campaigns]
+#     df = pd.concat(rows, ignore_index=True)
 
-with open("data/full_pull.csv", "w", encoding="utf-8") as fp:
-    df.to_csv(fp, index=False, line_terminator="\n", encoding="utf-8")
+# with open("data/full_pull.csv", "w", encoding="utf-8") as fp:
+#     df.to_csv(fp, index=False, line_terminator="\n", encoding="utf-8")
 
 # scraped = pd.read_csv("scraped_081822.csv", index_col=0, usecols=["URL", 'Text', 'Goal', 'Amount_Raised'])
 # assign most likely language
